6bde4c80d2fbf613d98f6bcb90229d66ebc256b4
alexnnygaard <143131337+alexnnygaard@users.noreply.github.com>
2024-02-13 15:51:37 +0100

    Test

---
+++ src/main/resources/kinobilletter.js
@@ -0,0 +1,23 @@
+function velgFilm(){
+    document.write("Valgt film: "+ document.getElementById("film").value);
+}
+
+function visAntall(){
+    let antall = document.getElementById("antall").value;
+}
+
+function visFornavn(){
+    let fornavn = document.getElementById("fornavn").value;
+}
+
+function visEtternavn(){
+    let etternavn = document.getElementById("etternavn").value;
+}
+
+function visTlfnr(){
+    let tlfnr = document.getElementById("tlfnr").value;
+}
+
+function visEpost(){
+    let epost = document.getElementById("epost").value;
+}

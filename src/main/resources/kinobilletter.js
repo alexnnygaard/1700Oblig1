bb3c72f5ad39774549dd567c7eba6646e538a24e
alexnnygaard <143131337+alexnnygaard@users.noreply.github.com>
2024-02-29 16:47:42 +0100

    fullstendig endring så det skal fungere (håper jeg)

--- src/main/resources/kinobilletter.js
+++ src/main/resources/kinobilletter.js
@@ -1,22 +1,8 @@
-function validerAntall(antall) {
-    const regexp = /^[0-9]{1,2}$/;
-    const ok = regexp.test(antall);
-    if (!ok) {
-        $("#feilAntall").html("Skriv inn antall billetter, max antall 99");
-        return false;
-    }
-    else {
-        $("#feilAntall").html("");
-        return true;
-    }
-}
-
-
-function validerFornavn(fornavn){
-    const regexp = /^[a-zA-ZæøåÆØÅ.\-]{1,20}$/;
-    const ok = regexp.test(fornavn);
-    if (!ok) {
-        $("#feilFornavn").html("Skriv inn fornavn, kun bruk bokstaver, må være mellom 2-20 bokstaver");
+function validerFornavn(Fornavn){
+    let regexp = /^[a-zA-ZæøåÆØÅ .\-]{2,20}$/;
+    let ok = regexp.test(Fornavn);
+    if(!ok) {
+        $("#feilFornavn").html("Fornavn må bestå av 2 til 20 bokstaver");
         return false;
     }
     else {
@@ -25,11 +11,35 @@ function validerFornavn(fornavn){
     }
 }
 
-function validerEtternavn(etternavn){
-    const regexp = /^[a-zA-ZæøåÆØÅ.\-]{1,30}$/;
-    const ok = regexp.test(etternavn);
-    if (!ok) {
-        $("#feilEtternavn").html("Skriv inn etternavn, kun bruk bokstaver, må være mellom 2-30 bokstaver");
+
+
+function validerAlternativer(Alternativ){
+    console.log(Alternativ)
+    if (Alternativ !== "Velg film her") {
+        $("#feilAlternativ").html("");
+        return true;
+    } else {
+        $("#feilAlternativ").html("Velg en av filmene");
+        return false;
+    }
+}
+function validerAntall(Antall){
+    let regexp = /^[0-9]{1,2}$/;
+    let ok = regexp.test(Antall);
+    if(!ok) {
+        $("#feilAntall").html("Skriv inn antall billetter, mellom 1-99");
+        return false;
+    }
+    else {
+        $("#feilAntall").html("");
+        return true;
+    }
+}
+function validerEtternavn(Etternavn){
+    let regexp = /^[a-zA-ZæøåÆØÅ .\-]{2,20}$/;
+    let ok = regexp.test(Etternavn);
+    if(!ok) {
+        $("#feilEtternavn").html("Skriv etternavn, mellom 2 til 20 bokstaver");
         return false;
     }
     else {
@@ -38,26 +48,24 @@ function validerEtternavn(etternavn){
     }
 }
 
-function validerTlfnr(tlfnr){
-    const regex = /^[0-9.]{8}$/;
-    const ok = regex.test(tlfnr);
-    if (!ok) {
-        $("#feilTlfnr").html("Skriv inn mobilnummer, 8 siffer");
+function validerTelefonnr(Telefonnr){
+    let regexp = /^[0-9. \-]{8}$/;
+    let ok = regexp.test(Telefonnr);
+    if(!ok) {
+        $("#feilTelefonnr").html("Ugyldig telefonnr, må bestå av åtte siffer");
         return false;
     }
-    else{
-        $("#feilTlfnr").html("");
+    else {
+        $("#feilTelefonnr").html("");
         return true;
     }
-
 }
 
-
-function validerEpost(epost){
-    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-    const ok = regex.test(epost);
-    if (!ok) {
-        $("#feilEpost").html("Skriv inn gyldig epost");
+function validerEpost(Epost){
+    let regexp = /^[a-zA-ZæøåÆØÅ0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let ok = regexp.test(Epost)
+    if(!ok) {
+        $("#feilEpost").html("Ugyldig epostadresse");
         return false;
     }
     else {
@@ -65,62 +73,75 @@ function validerEpost(epost){
         return true;
     }
 }
+function ValiderOgKjopbillett() {
+    let AlternativerOK = validerAlternativer($("#Alternativer").val());
+    let AntallBilletterOK = validerAntall($("#Antall").val());
+    let FornavnOK = validerFornavn($("#Fornavn").val());
+    let EtternavnOK = validerEtternavn($("#Etternavn").val());
+    let TelefonnrOK = validerTelefonnr($("#Telefonnr").val());
+    let EpostOK = validerEpost($("#Epost").val());
 
 
-
-let billettArray = [];
-function fyllArray() {
-
-    //Lager variabler ved å hente inn value fra html taggene jeg har laget.
-    let inputFilm = document.getElementById("film").value;
-    let inputAntall = document.getElementById("antall").value;
-    let inputFornavn = document.getElementById("fornavn").value;
-    let inputEtternavn = document.getElementById("etternavn").value;
-    let inputTlfnr = document.getElementById("tlfnr").value;
-    let inputEpost = document.getElementById("epost").value;
-
-    let billett1 = {     //Lager et objekt og setter inn lagde variabler.
-        film: inputFilm,
-        antall: inputAntall,
-        fornavn: inputFornavn,
-        etternavn: inputEtternavn,
-        telefonnummer: inputTlfnr,
-        epost: inputEpost
-    };
-
-
-    billettArray.push(billett1);
-    skrivUt();
-
-    //Henter verdiene som er oppe uten å sette det til noen variabler
-    document.getElementById("film").value = "";
-    document.getElementById("antall").value = "";
-    document.getElementById("fornavn").value = "";
-    document.getElementById("etternavn").value = "";
-    document.getElementById("tlfnr").value = "";
-    document.getElementById("epost").value = "";
-
-}
-
-function skrivUt() { //kjører gjennom en for-løkke for å skrive ut verdier vi har laget i objektet og satt inn i arrayet.
-    let ut = "";
-    for (let i = 0; i < billettArray.length; i++) {
-        ut += "Film: " + billettArray[i].film + " Antall: " + billettArray[i].antall + " Fornavn: " + billettArray[i].fornavn
-            + " Etternavn: " + billettArray[i].etternavn + " Tlfnummer: " + billettArray[i].telefonnummer +
-            " Epost: " + billettArray[i].epost + "<br>";
+    if (AlternativerOK && AntallBilletterOK && FornavnOK && EtternavnOK && TelefonnrOK && EpostOK) {
+        Addrow();
     }
-    document.getElementById("billettUtskrift").innerHTML = ut;
-
 }
 
+let billetter  = []
 
-
-
-function slettBillett() {
-    billettArray.splice(0, billettArray.length);
-
-    //billettArray = [];  // kunne også gjort det slik, tømmer arrayet ved å sette den tom.
-    console.log(billettArray); // sjekker i konsollen at arrayet er tømt når vi trykker på sletteknappen
-    skrivUt(); //sletter teksten som viser billettene
+class Billett{
+    constructor(film, antall, fornavn, etternavn, telefonnr, epost){
+        this.film=film
+        this.antall=antall
+        this.fornavn=fornavn
+        this.etternavn=etternavn
+        this.telefonnr=telefonnr
+        this.epost=epost
+    }
 }
 
+let n = 1;
+let x = 0;
+
+function Addrow(){
+    let Addrow = document.getElementById('Vis-Billett');
+    let NyRow = Addrow.insertRow(n);
+
+    const film = document.getElementById("Alternativer").value;
+    const antall = document.getElementById("Antall").value;
+    const fornavn = document.getElementById("Fornavn").value;
+    const etternavn= document.getElementById("Etternavn").value;
+    const telefonnr= document.getElementById("Telefonnr").value;
+    const epost= document.getElementById("Epost").value;
+
+    const billett=new Billett(film,antall,fornavn,etternavn,telefonnr,epost)
+    billetter.push(billett)
+
+
+    let cel1 = NyRow.insertCell(0);
+    let cel2=NyRow.insertCell(1);
+    let cel3=NyRow.insertCell(2);
+    let cel4=NyRow.insertCell(3);
+    let cel5=NyRow.insertCell(4);
+    let cel6=NyRow.insertCell(5);
+
+    cel1.innerHTML=billett.film;
+    cel2.innerHTML=billett.antall;
+    cel3.innerHTML=billett.fornavn;
+    cel4.innerHTML=billett.etternavn;
+    cel5.innerHTML=billett.telefonnr;
+    cel6.innerHTML=billett.epost;
+
+    n++;
+    x++;
+}
+
+function SlettBillett(){
+    let Addrow = document.getElementById('Vis-Billett');
+    for(i = x; i > 0; i--){
+        Addrow.deleteRow(i)
+    }
+    billetter=[];
+    n=1;
+    x=0;
+}
